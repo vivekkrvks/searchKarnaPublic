fa7010577484f74b1bd833171ef8e79a7beda95c
vivek kumar <vivek.kr.vks@gmail.com>
2022-04-09 15:47:43 +0530

    build

---
+++ client/build/precache-manifest.ce8a062e6931c42ff35adfb7fc25da97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0976e598d54608394da3d72506739d59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cc090fb2b8189f96754",
+    "url": "/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "21041a12a3a7c0fa7ed1",
+    "url": "/static/css/main.6777df22.chunk.css"
+  },
+  {
+    "revision": "4cc090fb2b8189f96754",
+    "url": "/static/js/2.cd1f5b72.chunk.js"
+  },
+  {
+    "revision": "45381e9c9aeb9f94c25f5c4ea3064e95",
+    "url": "/static/js/2.cd1f5b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21041a12a3a7c0fa7ed1",
+    "url": "/static/js/main.9a61df37.chunk.js"
+  },
+  {
+    "revision": "9d9a4c99211d98368a0d",
+    "url": "/static/js/runtime-main.7b3982eb.js"
+  }
+]);
